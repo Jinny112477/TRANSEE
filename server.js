66b38192604ee70286a44d09fef214c1ea6fbce2
Jinny1124 <pareena.dan@dome.tu.ac.th>
2025-11-15 01:55:20 +0700

    Fix PORT for Render deployment

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(session({
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
-app.use(express.static('components'));
 app.use('/src', express.static('src')); // Add this line
 app.use('/components', express.static(path.join(__dirname, 'components')));
 
